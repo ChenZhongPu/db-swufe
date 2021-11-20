6928a38c78f0f8551c935574ce755dcd8f986ac6
chenzhongpu <chenloveit@gmail.com>
2021-11-20 13:49:17 +0800

    add orm select

--- 10_advanced/python-orm/main-orm.py
+++ 10_advanced/python-orm/main-orm.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import registry, session
 mapper_registry = registry()
 Base = mapper_registry.generate_base()
 
-# 1. working with database metadata
+# working with database metadata
 from sqlalchemy import MetaData
 metadata_obj = MetaData()
 # register a table
@@ -18,27 +18,37 @@ dept_table = Table(
     Column('building', String),
     Column('budget', Float)
 )
-
+# orm
 class Department(Base):
     __table__ = dept_table
 
     def __repr__(self):
-        return f"Department({self.dept_name!r}"
+        return f"Department({self.dept_name!r})"
 
 
 
 from sqlalchemy.orm import Session
 
 # select
-from sqlalchemy import select
-stmt = select(Department).where(dept_table.c.building == 'Watson')
+# from sqlalchemy import select
+# stmt = select(Department).where(dept_table.c.building == 'Watson')
+# with Session(engine) as session:
+#     for row in session.execute(stmt):
+#         print(row)
+
+# orm select
 with Session(engine) as session:
-    for row in session.execute(stmt):
+    dept = session.query(Department).filter_by(building='Watson').first()
+    print(dept)
+    print('---')
+    depts = session.query(Department).filter(Department.dept_name.like('%s'))
+    for row in depts:
         print(row)
 
+
 # insert
-marx = Department(dept_name='Marx', building='Wastson', budget=80000.0)
-session = Session(engine)
-session.add(marx)
-session.flush()
-session.commit()
+# marx = Department(dept_name='Marx', building='Wastson', budget=80000.0)
+# session = Session(engine)
+# session.add(marx)
+# session.flush()
+# session.commit()
