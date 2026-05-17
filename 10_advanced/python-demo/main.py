e310998966b00e57be187fe7fc739711ca78b8b6
CHEN zhongpu <chenloveit@gmail.com>
2026-05-17 16:58:05 +0800

    update old package;

--- 10_advanced/python-demo/main.py
+++ 10_advanced/python-demo/main.py
@@ -1,6 +1,6 @@
-import psycopg2
+import psycopg
 
-with psycopg2.connect("dbname=mydb user=postgres port=5432 password=") as conn:
+with psycopg.connect("dbname=mydb user=postgres port=5432 password=") as conn:
     with conn.cursor() as cur:
         cur.execute("SELECT * FROM department")
         records = cur.fetchall()
